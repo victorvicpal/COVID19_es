a257fd9c5749772caa1b29ef48e844d55c597742
Victor Vicente Palacios <victorvicpal@gmail.com>
2020-03-16 19:07:32 +0100

    pdf_to_csv fixed

--- src/pdf_to_csv.py
+++ src/pdf_to_csv.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python
+
 from tika import parser
 import pandas as pd
 import re
+import sys, getopt
+import numpy as np
 
 ###############################################
 #                                             #
@@ -17,7 +21,7 @@ def get_fecha(string):
     return string[ind_ini:ind_fin].split('\n')[-1]+'2020'
 
 def parsing_table(string):
-    first_number = re.search(r'\d+', string).group()
+    first_number = re.search(r'[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?', string).group()
     new_lst = string.split(' ')
     ind = new_lst.index(first_number)
     final_list = [''.join(new_lst[:ind])]+new_lst[ind:]
@@ -27,11 +31,11 @@ def get_lst(string):
     ind_ini = string.lower().find('andalucía')
     ind_fin = string[ind_ini:].lower().find('total')
     list_tab = string[ind_ini:ind_ini+ind_fin].split('\n')
+    list_tab = [el.replace('.','') for el in [el.rstrip() for el in list_tab] if el != '']
     list_tab = [el.replace(',','.') for el in [el.rstrip() for el in list_tab] if el != '']
     return [parsing_table(el) for el in list_tab]
 
-def save_csv(lst, path):
-    lst = get_lst(raw['content'])
+def save_csv(lst,fecha, path):
     if len(lst[0])==5:
         cols = ['CCAA', 'casos', 'IA', 'UCI', 'muertes']
     elif len(lst[0])==4:
@@ -45,20 +49,20 @@ def save_csv(lst, path):
     
 def main(argv):
     inputfile = ''
-    outputfile = ''
     try:
         opts, args = getopt.getopt(argv,"i:",["ifile="])
     except getopt.GetoptError:
-        print('test.py -i <inputfile>')
+        print('pdf_to_csv.py -i <inputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-i", "--ifile"):
             inputfile = arg
-    
-    raw = parser.from_file(inputfile)
-    fecha = get_fecha(raw['content'])
-    lst = get_lst(raw['content'])
-    save_csv(lst,'../data/csv_data/COVID_es_{}.csv'.format(fecha.replace('.','_')))
+
+    rawdata = parser.from_file(inputfile)
+    fecha = get_fecha(rawdata['content'])
+    lst = get_lst(rawdata['content'])
+    save_csv(lst, fecha,'../data/csv_data/COVID_es_{}.csv'.format(fecha.replace('.','_')))
+    print('COVID_es_{}.csv created'.format(fecha.replace('.','_')))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
